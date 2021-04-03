cd73e2a642920bb8f43bf7b438d2d55d4ba9c7ab
MarvelousMarcin <01159356@pw.edu.pl>
2021-04-03 12:29:50 +0200

    Dodanie implementacji cyklu

---
+++ src/one_cycle.h
@@ -0,0 +1,7 @@
+#ifndef ONE_CYCLE_H
+#define ONE_CYCLE_H
+
+void cycle (int** X, int** Y, int** V, int *sizeF, int *sizeA, int rows, int columns);
+
+
+#endif
--- src/unit_test_1.c
+++ src/unit_test_1.c
@@ -2,52 +2,32 @@
 #include <stdlib.h>
 #include "sparse_matrix.h"
 #include "output_to_file.h"
+#include "one_cycle.h"
 
 #define REALLOC_SIZE 3
 
 int main(int argc, char *argv[]) {
+    char *filename = "../init_boards/ut_1.txt";
+    FILE* in = fopen(filename,"r");
+    int rows, columns;
+    fscanf(in,"%d %d", &rows, &columns);
 
     int cycle_number = 0;
-    FILE* in = fopen("../init_boards/ut_1.txt", "r");
-    int rows, columns;
-
-    fscanf(in, "%d %d", &rows, &columns);
-	
-	
     int sizeFilled = 0;                           //fun fact: mozna zaalokowac pamiec na 0 bajtow, kocham C
     int sizeAllocated = REALLOC_SIZE;
     int *X = (int*) malloc(sizeof(int) * REALLOC_SIZE);  //wspolzedne x komorek zywych lub przeszkod
     int *Y = (int*) malloc(sizeof(int) * REALLOC_SIZE);  //wspolzedne y komorek zywych lub przeszkod
     int *V = (int*) malloc(sizeof(int) * REALLOC_SIZE);  //wartosci komorek (determinuje czy mamy do czynienia z przeszkoda czy zywa komorka)
 
-    init_from_file(&X, &Y, &V, &sizeFilled, &sizeAllocated, "../init_boards/ut_1.txt"); //jak w jakiejs funkcji jest realloc albo cos takiego to trzeba tak przekazywac
+    init_from_file(&X, &Y, &V, &sizeFilled, &sizeAllocated, filename); //jak w jakiejs funkcji jest realloc albo cos takiego to trzeba tak przekazywac
 
-    printf("sizeF = %d\n", sizeFilled);
-    printf("sizeA = %d\n", sizeAllocated);
-
-    printf("    X Y V\n");
-	for (int i = 0; i < sizeFilled; i++) {
-        printf("K%d: %d %d %d\n", i+1, X[i], Y[i], V[i]);
+    for(int i=0;i<10;i++) {
+        cycle(&X,&Y,&V,&sizeFilled,&sizeAllocated,rows,columns);
+        output_to_txt(X,Y,V, sizeFilled, rows,columns,cycle_number);
+        cycle_number++;
     }
 
-    printf("Usuwamy komorke K13 (5, 0) i K5 (1, 4)\n");
-    remove_cell(&X, &Y, &V, 5, 0, &sizeFilled, &sizeAllocated);
-    remove_cell(&X, &Y, &V, 1, 4, &sizeFilled, &sizeAllocated);
-    dealloc_if_needed(&X, &Y, &V, &sizeFilled, &sizeAllocated);
 
-    printf("sizeF = %d\n", sizeFilled);
-    printf("sizeA = %d\n", sizeAllocated);
-
-    printf("    X Y V\n");
-    for (int i = 0; i < sizeFilled; i++) {
-        printf("K%d: %d %d %d\n", i + 1, X[i], Y[i], V[i]);
-    }
-
-    int sasiedztwoK3 = count_alive(X, Y, V, 4, 5, &sizeFilled);
-    int sasiedztwoK4 = count_alive(X, Y, V, 4, 6, &sizeFilled); // tak zrobilem zeby bylo czytelniej niz to printfa wrzucic xd
-    printf("Liczba zywych komorek w sasiedztwie K3: %d, oraz K4: %d\n", sasiedztwoK3, sasiedztwoK4);
-
-    output_to_txt(X,Y,V, sizeFilled, rows,columns,cycle_number);
-
-	return 0;
+    return 0;
 }
+
